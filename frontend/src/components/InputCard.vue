54ca052fe7000b98a28e297c1250a95fa9baa7bb
habeeb <habeebomotolani2007@gmail.com>
2025-03-03 09:28:22 +0100

    added clear button

--- frontend/src/components/InputCard.vue
+++ frontend/src/components/InputCard.vue
@@ -21,6 +21,11 @@ const closeError = () => {
     translatorStore.showTranscriptError = false
 }
 
+const clearInput = () => {
+    userInput.value = ''
+    translatorStore.userInput = ''
+}
+
 watch(
     () => translatorStore.audioTranscript,
     (newVal, oldVal) => {
@@ -55,15 +60,22 @@ watch(
             />
         </div>
         <div class="action-container">
-        <button :class="`speech-button ${translatorStore.activateMic ? 'active' : ''}`" @click="toggleMic">
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
-                <path d="M480-400q-50 0-85-35t-35-85v-240q0-50 35-85t85-35q50 0 85 35t35 85v240q0 50-35 85t-85 35Zm0-240Zm-40 520v-123q-104-14-172-93t-68-184h80q0 83 58.5 141.5T480-320q83 0 141.5-58.5T680-520h80q0 105-68 184t-172 93v123h-80Zm40-360q17 0 28.5-11.5T520-520v-240q0-17-11.5-28.5T480-800q-17 0-28.5 11.5T440-760v240q0 17 11.5 28.5T480-480Z"/>
-            </svg>
-        </button>
-        <Spinner
-            :onlyChild="false" 
-            v-if="translatorStore.activateMic" 
-        />
+            <button :class="`speech-button ${translatorStore.activateMic ? 'active' : ''}`" @click="toggleMic">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+                    <path d="M480-400q-50 0-85-35t-35-85v-240q0-50 35-85t85-35q50 0 85 35t35 85v240q0 50-35 85t-85 35Zm0-240Zm-40 520v-123q-104-14-172-93t-68-184h80q0 83 58.5 141.5T480-320q83 0 141.5-58.5T680-520h80q0 105-68 184t-172 93v123h-80Zm40-360q17 0 28.5-11.5T520-520v-240q0-17-11.5-28.5T480-800q-17 0-28.5 11.5T440-760v240q0 17 11.5 28.5T480-480Z"/>
+                </svg>
+            </button>
+            <div class="action-buttons">
+                <Spinner
+                    :onlyChild="false" 
+                    v-if="translatorStore.activateMic" 
+                />
+                <button class="clear-button" v-if="translatorStore.userInput" @click="clearInput">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#D3D3D4">
+                        <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+                    </svg>
+                </button>
+            </div>
         </div>
     </TranslatorContainer>
 </template>
@@ -99,7 +111,12 @@ watch(
     justify-content: space-between;
 }
 
-.speech-button { 
+.action-buttons {
+    display: flex;
+    gap: 20px;
+}
+
+.speech-button, .clear-button { 
     position: relative;
     border: none;
     background-color: #252427;
@@ -128,7 +145,7 @@ watch(
     border-radius: 30px;
 }
 
-.speech-button:hover {
+.speech-button:hover, .clear-button:hover {
     background-color: #39435c;
 }
 
